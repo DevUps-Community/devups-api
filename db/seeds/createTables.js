6702ae2af75c6501ec6290111065ce1ac7c002e2
munaciella <franes88@gmail.com>
2024-06-26 19:10:58 +0100

    add additinal columns to tables

--- db/seeds/createTables.js
+++ db/seeds/createTables.js
@@ -4,13 +4,17 @@ exports.createTables = async () => {
     await db.query(`CREATE TABLE categories (
         category_id SERIAL PRIMARY KEY,
         name VARCHAR(255) UNIQUE NOT NULL,
-        description VARCHAR(255) NOT NULL
+        description VARCHAR(255) NOT NULL,
+        created_by VARCHAR(30),
+        created_at TIMESTAMPTZ
     );`);
     await db.query(`CREATE TABLE topics (
         topic_id SERIAL PRIMARY KEY,
         category_id INT UNIQUE NOT NULL,
         name VARCHAR(255) UNIQUE,
         description VARCHAR(255) NOT NULL,
+        created_by VARCHAR(30),
+        created_at TIMESTAMPTZ,
         FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
     );`);
     await db.query(`CREATE TABLE tags (
@@ -22,7 +26,10 @@ exports.createTables = async () => {
         title VARCHAR(255) UNIQUE NOT NULL,
         content TEXT,
         category_id INT NOT NULL,
-        topic_id INT NOT NULL, 
+        topic_id INT NOT NULL,
+        visible BOOL,
+        created_by VARCHAR(30),
+        created_at TIMESTAMPTZ,
         FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
     );`);
     await db.query(`CREATE TABLE note_tags (
