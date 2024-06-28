1487580a8ded902ba3ac47bebeb91e12b09145af
munaciella <franes88@gmail.com>
2024-06-28 22:51:57 +0100

    add some initial dev-data

---
+++ db/data/dev-data/categories.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    name: 'JavaScript',
+    description: 'A popular programming language used for web development.',
+  },
+  {
+    name: 'Python',
+    description: 'A popular programming language used for web development.',
+  },
+  {
+    name: 'Ruby',
+    description: 'A popular programming language used for web development.',
+  },
+  {
+    name: 'Java',
+    description: 'A popular programming language used for web development.',
+  }
+];
