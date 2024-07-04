f39069b74b5e31b50e57ab6d6b543c6480c5dbae
munaciella <franes88@gmail.com>
2024-07-04 19:14:15 +0100

    create endpoint for fetching categories

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
 const app = express();
-
 app.use(express.json());
+
+app.get("/api/categories", getCategories)
+
+app.use((err, req, res) => {
+    console.log(err, '<--- error in app js');
+    const status = err.status || 500
+    const message = err.msg || "Internal Server Error"
+    res.status(status).send({msg: message})
+})
 
 
 module.exports = app;
